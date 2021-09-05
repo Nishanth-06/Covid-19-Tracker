e5132e49a5d8f496d5c77a835e914fef9622a876
Nishanth-06 <67260068+Nishanth-06@users.noreply.github.com>
2021-09-05 10:43:42 +0530

    initial commit

---
+++ src/LineGraph.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import numeral from "numeral";
+
+const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format("+0,0");
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          format: "MM/DD/YY",
+          tooltipFormat: "ll",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          // Include a dollar sign in the ticks
+          callback: function (value, index, values) {
+            return numeral(value).format("0a");
+          },
+        },
+      },
+    ],
+  },
+};
+
+const buildChartData = (data, casesType) => {
+  let chartData = [];
+  let lastDataPoint;
+  for (let date in data.cases) {
+    if (lastDataPoint) {
+      let newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint,
+      };
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+};
+
+function LineGraph({ casesType, ...props }) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          let chartData = buildChartData(data, casesType);
+          setData(chartData);
+          console.log(chartData);
+          // buildChart(chartData);
+        });
+    };
+
+    fetchData();
+  }, [casesType]);
+// https://codesandbox.io/s/react-chartjs-2-line-chart-configuration-example-forked-bzchd?file=/src/App.js
+  const legend = {
+    display: true,
+    position: "top",
+    labels: {
+      fontColor: "#323130",
+      fontSize: 14
+    }
+  }
+
+  return (
+    <div className={props.className}>
+      {data?.length > 0 && (
+        <Line
+          data={{
+            datasets: [
+              {
+                label:"cases",
+                backgroundColor: "rgb(204, 16, 52, 0.5)",
+                borderColor: "#CC1034",
+                data: data,
+                fill:true,
+              },
+            ],
+          }}
+          legend={legend}
+          options={options}
+        />
+      )}
+    </div>
+  );
+}
+
+export default LineGraph;
